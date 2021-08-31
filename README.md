4b8cc748362c8d3903ca8f840ceb5c5ac69b514a
wywy136 <48551697+wywy136@users.noreply.github.com>
2021-08-31 21:34:12 +0800

    0831

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# StrategyQA
+# Disentangled Retrieval and Reasoning for Implicit Question Answering
 
-### 代码结构
+This repository contains codes for the paper "Disentangled Retrieval and Reasoning for Implicit Question Answering". 
 
-- data/：数据
-- dataset/：Dataset类
-- golden_sentence_predictor/：预测关键句的model
-- model/：Reasoning模型
-- trainer/：训练
-- config.py：配置
-- evaluator.py：评价
-- main.py
-- 其他文件没用
+### Running
 
-### 数据
+#### Dependencies
 
-数据都在data/路径下，其中strategyqa/路径下存放的4个文件是官网提供的训练数据和测试数据，其他json文件分别为
+```
+python==3.8
+torch==1.9.0
+nltk==3.6.8
+transformers==4.9.0
+```
 
-- dev：论文作者从train中划分出来的验证集
-- train：论文作者从train中划分出来的小train
-- dev_gdsent：包含预测出的golden sentence的dev
-- train_gdsent：包含预测出的golden sentenced的小train
-- transformer_qa_ORA-P_dev_no_placeholders：将dev中的占位符都替换为预测结果，复现last-step那个模型用
-- transformer_qa_ORA-P_train_no_placeholders：同上
+#### Baseline
 
-### 论文里的model
+The weight model named `weights.th` of baseline should be in the path `./pretrained_model/6_STAR_ORA-p/`, which could be downloaded and unzipped from [here](https://storage.googleapis.com/ai2i/strategyqa/models/6_STAR_ORA-P.tar.gz).
 
-论文的github链接：[eladsegal/strategyqa: The official code of TACL 2021, "Did Aristotle Use a Laptop? A Question Answering Benchmark with Implicit Reasoning Strategies". (github.com)](https://github.com/eladsegal/strategyqa)
+#### Configuration
 
-其中提供了很多checkpoint，包括用BART做分解的那个model
+Run the model with default configuration
+
+```
+python main.py
+```
+
+Configuration can be edited in the file  `main.py` or in the running command line, for example, 
+
+```
+python main.py \
+--num_workers 0 \ 
+--load_pretrained true \ 
+--epoch_num 20 \ 
+--batch_size 16
+```
+
+### Operator
+
+The json files in the path `./classification/` describes several strategies for the definition and classification of operators, which are crucial components in our reasoning. In the paper, we adopt the 5-class strategy, that is, *comparison*, *logical*, *entail*, *numerical* and *binary*. To try another classification strategy, change the configuration `--op_classification` accordingly.
+
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import argparse
 from trainer.trainer import Trainer
-from trainer.pretrainer import Pretrainer
-from trainer.golden_sentence_trainer import GoldenSentenceTrainer
-from golden_sentence_predictor.squad import SquadGoldenSentencePredictor
 
 
 def main(args):
@@ -11,12 +8,6 @@ def main(args):
     t.train()
     # t.pretrain()
     t.save()
-    # m = GoldenSentenceTrainer()
-    # m.load_pretrained()
-    # m.train()
-    # m.save()
-    # s = SquadGoldenSentencePredictor('dev')
-    # s.predict()
 
 
 if __name__ == '__main__':
@@ -24,7 +15,7 @@ if __name__ == '__main__':
     parser.add_argument('--pretrained_model_path', type=str, default='./pretrained_model/6_STAR_ORA-P/weights.th')
     parser.add_argument('--model_path', type=str, default='./checkpoints/combination/1.pth')
     parser.add_argument('--trained_model_path', type=str, default='./checkpoints/combination/1.pth')
-    parser.add_argument('--model_class', type=str, default="ReasoningSelfAttention")
+    parser.add_argument('--model_class', type=str, default="ReasoningPlain")
 
     parser.add_argument('--train_path', type=str, default="./data/train_gdsent.json")  # data/transformer_qa_ORA-P_train_no_placeholders.json
     parser.add_argument('--dev_path', type=str, default="./data/dev_gdsent.json")
@@ -40,20 +31,10 @@ if __name__ == '__main__':
     parser.add_argument('--fields', type=list, default= ["question", "evidence"])
     parser.add_argument('--max_length', type=int, default=512)
 
-    # boolq_path: str = './data/boolq/train.jsonl'
-    # twentyquestion_path: str = './data/twentyquestions/v1.0.twentyquestions.tar'
-    # squad_train_path: str = './data/squad/train-v2.0.json'
-    # squad_dev_path: str = './data/squad/dev-v2.0.json'
-
     parser.add_argument('--cuda', type=bool, default=False)
     parser.add_argument('--num_workers', type=int, default=0)
     parser.add_argument('--load_pretrained', type=bool, default=True)
     parser.add_argument('--eval_only', type=bool, default=False)
-
-    # pretrain_epoch_num_boolq: int = 20
-    # pretrain_epoch_num_20q: int = 20
-    # pretrain_batch_size: int = 8
-    # pretrain_learning_rate: float = 1e-5
 
     parser.add_argument('--epoch_num', type=int, default=20)
     parser.add_argument('--batch_size', type=int, default=1)
